e0d3b5259bee04b6892d04e606a8795c76180415
norppa <norppa@mailinator.com>
2020-12-11 13:49:03 +0200

    add morgan

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
 
 const app = express()
 app.use(cors())
+app.use(morgan('short'))
 app.use(express.json())
 
 app.use('/cocktailindex', require('./modules/CocktailIndexServer/routers/router'))
